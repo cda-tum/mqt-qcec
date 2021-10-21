3b71e44702ba925c1ad8a104bc725d21ac1dde34
Lukas Burgholzer <lukas.burgholzer@jku.at>
2021-10-21 13:50:45 +0200

    ⚡ actually enables -march=native -mtune=native for source builds via pip
    Signed-off-by: Lukas Burgholzer <lukas.burgholzer@jku.at>

--- setup.py
+++ setup.py
@@ -33,8 +33,7 @@ class CMakeBuild(build_ext):
 
         cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
                       '-DPYTHON_EXECUTABLE=' + sys.executable,
-                      '-DBINDINGS=ON',
-                      '-DDEPLOY=ON']
+                      '-DBINDINGS=ON']
 
         cfg = 'Debug' if self.debug else 'Release'
         build_args = ['--config', cfg]
