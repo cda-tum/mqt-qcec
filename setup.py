fe9d4692d59a4a37b6bec2b9ed8e597ecb253233
Lukas Burgholzer <lukas.burgholzer@jku.at>
2021-10-21 15:49:35 +0200

    Version 1.10.1 (#9)
     - ✨/🐛 now natively supports ancillary registers and c3x/c4x gates in Qiskit QuantumCircuit objects
     - ⚡ actually enables -march=native -mtune=native for source builds via pip
     - ⚡ tunes the setup.py script to run on as many threads as available
     - 🚀 adds capability to build Python 3.10 wheels
     - 📝 updates documentation
     - 📌 increases the version number to 1.10.1

--- setup.py
+++ setup.py
@@ -33,8 +33,7 @@ class CMakeBuild(build_ext):
 
         cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
                       '-DPYTHON_EXECUTABLE=' + sys.executable,
-                      '-DBINDINGS=ON',
-                      '-DDEPLOY=ON']
+                      '-DBINDINGS=ON']
 
         cfg = 'Debug' if self.debug else 'Release'
         build_args = ['--config', cfg]
@@ -47,7 +46,10 @@ class CMakeBuild(build_ext):
             build_args += ['--', '/m']
         else:
             cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
-            build_args += ['--', '-j2']
+            cpus = os.cpu_count()
+            if cpus is None:
+                cpus = 2
+            build_args += ['--', '-j{}'.format(cpus)]
 
         env = os.environ.copy()
         env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(env.get('CXXFLAGS', ''),
@@ -65,7 +67,7 @@ with open(README_PATH) as readme_file:
 
 setup(
     name='jkq.qcec',
-    version='1.10.0',
+    version='1.10.1',
     author='Lukas Burgholzer',
     author_email='lukas.burgholzer@jku.at',
     description='QCEC - A JKQ tool for Quantum Circuit Equivalence Checking',
