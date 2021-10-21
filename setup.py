7d881ce91371e5c3394233c6f891b3a69de1a544
Lukas Burgholzer <lukas.burgholzer@jku.at>
2021-10-21 13:48:48 +0200

    ⚡ tunes the setup.py script to run on as many threads as available
    Signed-off-by: Lukas Burgholzer <lukas.burgholzer@jku.at>

--- setup.py
+++ setup.py
@@ -47,7 +47,10 @@ class CMakeBuild(build_ext):
             build_args += ['--', '/m']
         else:
             cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
-            build_args += ['--', '-j2']
+            cpus = os.cpu_count()
+            if cpus is None:
+                cpus = 2
+            build_args += ['--', '-j{}'.format(cpus)]
 
         env = os.environ.copy()
         env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(env.get('CXXFLAGS', ''),
