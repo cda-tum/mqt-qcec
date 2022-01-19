ef51823fb1084c42eaa451a0df87b4c28f82058a
Lukas Burgholzer <lukas.burgholzer@jku.at>
2022-01-19 17:35:45 +0100

    📝 trying to fix CI for documentation
    Signed-off-by: Lukas Burgholzer <lukas.burgholzer@jku.at>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,7 +5,9 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath('../../jkq/qcec'))
+sys.path.insert(0, os.path.abspath('../jkq/qcec'))
 sys.path.insert(0, os.path.abspath('../../jkq/'))
+sys.path.insert(0, os.path.abspath('../jkq/'))
 
 # -- Project information -----------------------------------------------------
 project = 'QCEC'
