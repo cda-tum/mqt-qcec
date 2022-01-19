e98ba0112fde283b955ec74d5b78fc40efdb21e2
Lukas Burgholzer <lukas.burgholzer@jku.at>
2022-01-19 17:48:32 +0100

    📝 deleted the wrong lines
    Signed-off-by: Lukas Burgholzer <lukas.burgholzer@jku.at>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,7 +5,7 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath('../../jkq/qcec'))
-sys.path.insert(0, os.path.abspath('../jkq/qcec'))
+sys.path.insert(0, os.path.abspath('../../jkq/'))
 
 # -- Project information -----------------------------------------------------
 project = 'QCEC'
