50933f40fa834578eb58fde53902e76d5ad65c4e
Lukas Burgholzer <lukas.burgholzer@jku.at>
2022-01-20 17:23:05 +0100

    📝 add code copy button to documentation
    Signed-off-by: Lukas Burgholzer <lukas.burgholzer@jku.at>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,14 +11,20 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.intersphinx",
     "sphinx.ext.autosummary",
+    "sphinx.ext.mathjax",
     "sphinx.ext.napoleon",
     "sphinx.ext.viewcode",
     "sphinx.ext.githubpages",
     "sphinxcontrib.bibtex",
+    "sphinx_copybutton"
 ]
 
 bibtex_bibfiles = ['refs.bib']
 bibtex_reference_style = 'author_year'
+
+copybutton_prompt_text = r"(?:\(venv\) )?\$ "
+copybutton_prompt_is_regexp = True
+copybutton_line_continuation_character = "\\"
 
 autosummary_generate = True
 
