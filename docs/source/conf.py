3933127d25aa82be0d4c86ef6032ce410ed0c8bc
Lukas Burgholzer <lukas.burgholzer@jku.at>
2022-01-25 17:53:31 +0100

    📝 documentation infrastructure
    Signed-off-by: Lukas Burgholzer <lukas.burgholzer@jku.at>

---
+++ docs/source/conf.py
@@ -0,0 +1,32 @@
+# -- Project information -----------------------------------------------------
+project = 'QCEC'
+copyright = '2022, Lukas Burgholzer'
+author = 'Lukas Burgholzer'
+
+# The full version, including alpha/beta/rc tags
+release = '1.10.4'
+
+# -- General configuration ---------------------------------------------------
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.githubpages",
+    "sphinxcontrib.bibtex",
+    "sphinx_copybutton"
+]
+
+bibtex_bibfiles = ['refs.bib']
+bibtex_reference_style = 'author_year'
+
+copybutton_prompt_text = r"(?:\(venv\) )?\$ "
+copybutton_prompt_is_regexp = True
+copybutton_line_continuation_character = "\\"
+
+autosummary_generate = True
+
+# -- Options for HTML output -------------------------------------------------
+html_theme = 'sphinx_rtd_theme'
---
+++ docs/source/index.rst
@@ -0,0 +1,4 @@
+Welcome to QCEC's documentation!
+================================
+
+At the moment, this documentation is just blank. Expect this to change in the near future.
