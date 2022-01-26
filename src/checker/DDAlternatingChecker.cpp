e3012d619df1cec0197f0641e0b93f70eed3c9a4
Lukas Burgholzer <lukas.burgholzer@jku.at>
2022-01-26 16:37:54 +0100

    ✅ some more hard to cover lines
    Signed-off-by: Lukas Burgholzer <lukas.burgholzer@jku.at>

--- src/checker/DDAlternatingChecker.cpp
+++ src/checker/DDAlternatingChecker.cpp
@@ -140,30 +140,22 @@ namespace ec {
         const auto& op2 = *taskManager2();
 
         // check type
-        if (op1.getType() != op2.getType()) {
-            return false;
-        }
+        if (op1.getType() != op2.getType()) { return false; }
 
         // check number of controls
         const auto nc1 = op1.getNcontrols();
         const auto nc2 = op2.getNcontrols();
-        if (nc1 != nc2) {
-            return false;
-        }
+        if (nc1 != nc2) { return false; }
 
         // SWAPs are not handled by this routine
-        if (op1.getType() == qc::SWAP && nc1 == 0U) {
-            return false;
-        }
+        if (op1.getType() == qc::SWAP && nc1 == 0U) { return false; }
 
         // check parameters
         const auto param1 = op1.getParameter();
         const auto param2 = op2.getParameter();
         for (std::size_t p = 0U; p < qc::MAX_PARAMETERS; ++p) {
             // it might make sense to use fuzzy comparison here
-            if (param1[p] != param2[p]) {
-                return false;
-            }
+            if (param1[p] != param2[p]) { return false; }
         }
 
         // check controls
@@ -176,9 +168,7 @@ namespace ec {
             for (const auto& control: op2.getControls()) {
                 controls2.emplace(dd::Control{taskManager2.getPermutation().at(control.qubit), control.type});
             }
-            if (controls1 != controls2) {
-                return false;
-            }
+            if (controls1 != controls2) { return false; }
         }
 
         // check targets
@@ -190,9 +180,7 @@ namespace ec {
         for (const auto& target: op2.getTargets()) {
             targets2.emplace(taskManager2.getPermutation().at(target));
         }
-        if (targets1 != targets2) {
-            return false;
-        }
+        if (targets1 != targets2) { return false; }
 
         // operations are identical
         return true;
