0407e9a65bcbc23cffc8973eac437197ea59cc76
Lukas Burgholzer <lukas.burgholzer@jku.at>
2022-01-27 23:48:09 +0100

    ⚡ simplify lookahead application scheme
    Signed-off-by: Lukas Burgholzer <lukas.burgholzer@jku.at>

--- src/checker/dd/applicationscheme/LookaheadApplicationScheme.cpp
+++ src/checker/dd/applicationscheme/LookaheadApplicationScheme.cpp
@@ -33,53 +33,23 @@ namespace ec {
 
         // greedily chose the smaller resulting decision diagram
         if (size1 <= size2) {
+            assert(!taskManager1.finished());
             *internalState = dd1;
             package->decRef(op1);
             cached1 = false;
+            taskManager1.advanceIterator();
         } else {
+            assert(!taskManager2.finished());
             *internalState = dd2;
             package->decRef(op2);
             cached2 = false;
+            taskManager2.advanceIterator();
         }
 
         // properly track reference counts
         package->incRef(*internalState);
         package->decRef(saved);
         package->garbageCollect();
-
-        if (size1 <= size2) {
-            if (taskManager1.finished()) {
-                // properly handle any cached operations on exit
-                if (cached2) {
-                    saved          = *internalState;
-                    *internalState = package->multiply(saved, op2);
-                    package->incRef(*internalState);
-                    package->decRef(saved);
-                    package->decRef(op2);
-                    cached2 = false;
-                    package->garbageCollect();
-                }
-                return {0U, 0U};
-            } else {
-                taskManager1.advanceIterator();
-            }
-        } else {
-            if (taskManager2.finished()) {
-                // properly handle any cached operations on exit
-                if (cached1) {
-                    saved          = *internalState;
-                    *internalState = package->multiply(op1, saved);
-                    package->incRef(*internalState);
-                    package->decRef(saved);
-                    package->decRef(op1);
-                    cached1 = false;
-                    package->garbageCollect();
-                }
-                return {0U, 0U};
-            } else {
-                taskManager2.advanceIterator();
-            }
-        }
 
         // no operations shall be applied by the outer loop in which the application scheme is invoked
         return {0U, 0U};
