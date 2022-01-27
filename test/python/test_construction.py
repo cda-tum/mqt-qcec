f1fd7ad2a66c4ce45e4093fed72926623ed2fed8
Lukas Burgholzer <lukas.burgholzer@jku.at>
2022-01-27 18:32:59 +0100

    ✅ initial commit for python tests
    Signed-off-by: Lukas Burgholzer <lukas.burgholzer@jku.at>

---
+++ test/python/test_construction.py
@@ -0,0 +1,31 @@
+import unittest
+
+from jkq import qcec
+from qiskit import QuantumCircuit
+
+
+class QCECConstructionTests(unittest.TestCase):
+    def setUp(self):
+        # create a very simple quantum circuit
+        self.qc1 = QuantumCircuit(4)
+        self.qc1.h(0)
+        # create a second (identical) circuit
+        self.qc2 = QuantumCircuit(4)
+        self.qc2.h(0)
+
+        self.file1 = "../circuits/original/mlp4_245.real"
+        self.file2 = "../circuits/transpiled/mlp4_245_transpiled.qasm"
+
+        self.config = qcec.Configuration()
+
+    def test_default_constructor_with_qiskit(self):
+        """Test constructing an instance from two qiskit circuits with all default arguments"""
+        qcec.EquivalenceCheckingManager(circ1=self.qc1, circ2=self.qc2)
+
+    def test_default_constructor_with_file(self):
+        """Test constructing an instance from two circuit files with all default arguments"""
+        qcec.EquivalenceCheckingManager(circ1=self.file1, circ2=self.file2)
+
+    def test_constructor_with_configuration(self):
+        """Test constructing an instance from circuits and a configuration object"""
+        qcec.EquivalenceCheckingManager(circ1=self.qc1, circ2=self.qc2, config=self.config)
