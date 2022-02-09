a8ea94cb2ed8bf31579e38ec3596c968c23d456f
Lukas Burgholzer <lukas.burgholzer@jku.at>
2022-02-09 12:03:44 +0100

    ♻️ refactor legacy gate cost function
    Signed-off-by: Lukas Burgholzer <lukas.burgholzer@jku.at>

--- src/checker/dd/applicationscheme/GateCostApplicationScheme.cpp
+++ src/checker/dd/applicationscheme/GateCostApplicationScheme.cpp
@@ -68,17 +68,50 @@ namespace ec {
 
     std::size_t LegacyIBMCostFunction(const GateCostLUTKeyType& key) noexcept {
         const auto [gate, nc] = key;
-        switch (gate) {
-            case qc::I:
-                return 1;
 
-            case qc::X:
-                if (nc <= 1U) {
+        if (nc == 0U) {
+            switch (gate) {
+                case qc::I:
+                case qc::H:
+                case qc::X:
+                case qc::Y:
+                case qc::Z:
+                case qc::S:
+                case qc::Sdag:
+                case qc::T:
+                case qc::Tdag:
+                case qc::Phase:
+                case qc::U2:
+                case qc::U3:
+                case qc::SX:
+                case qc::SXdag:
+                case qc::V:
+                case qc::Vdag:
+                case qc::RX:
+                case qc::RY:
+                case qc::RZ:
+                // the following are merely placeholders so that the check can advance
+                case qc::Compound:
+                case qc::Measure:
+                case qc::Reset:
+                case qc::Snapshot:
+                case qc::ShowProbabilities:
+                case qc::Barrier:
+                case qc::ClassicControlled:
                     return 1U;
-                } else {
-                    return 2U * (nc - 2U) * (2U * LegacyIBMCostFunction({qc::Phase, 0}) + 2 * LegacyIBMCostFunction({qc::U2, 0}) + 3U * LegacyIBMCostFunction({qc::X, 1})) + 6U * LegacyIBMCostFunction({qc::X, 1}) + 8U * LegacyIBMCostFunction({qc::U3, 0});
-                }
+                default:
+                    break;
+            }
+        }
 
+        // special treatment for CNOT
+        if (gate == qc::X && nc == 1) {
+            return 1U;
+        }
+
+        switch (gate) {
+            case qc::X:
+                return 2U * (nc - 2U) * (2U * LegacyIBMCostFunction({qc::Phase, 0}) + 2 * LegacyIBMCostFunction({qc::U2, 0}) + 3U * LegacyIBMCostFunction({qc::X, 1})) + 6U * LegacyIBMCostFunction({qc::X, 1}) + 8U * LegacyIBMCostFunction({qc::U3, 0});
             case qc::U3:
             case qc::U2:
             case qc::V:
@@ -88,54 +121,25 @@ namespace ec {
             case qc::H:
             case qc::SX:
             case qc::SXdag:
-                if (nc == 0U) {
-                    return 1U;
-                }
-                if (nc == 1U) {
-                    return 2U * LegacyIBMCostFunction({qc::X, 1}) + 4U * LegacyIBMCostFunction({qc::U3, 0});
-                } else {
-                    return 2U * LegacyIBMCostFunction({qc::X, nc}) + 4U * LegacyIBMCostFunction({qc::U3, 0}); // heuristic
-                }
+                return 2U * LegacyIBMCostFunction({qc::X, nc}) + 4U * LegacyIBMCostFunction({qc::U3, 0}); // heuristic
             case qc::Phase:
             case qc::S:
             case qc::Sdag:
             case qc::T:
             case qc::Tdag:
             case qc::RZ:
-                if (nc == 0U) {
-                    return 1U;
-                }
-                if (nc == 1U) {
-                    return 2U * LegacyIBMCostFunction({qc::X, 1}) + 3U * LegacyIBMCostFunction({qc::Phase, 0});
-                } else {
-                    return 2U * LegacyIBMCostFunction({qc::X, nc}) + 3U * LegacyIBMCostFunction({qc::U3, 0}); // heuristic
-                }
+                return 2U * LegacyIBMCostFunction({qc::X, nc}) + 3U * LegacyIBMCostFunction({qc::Phase, 0}); // heuristic
             case qc::Y:
             case qc::Z:
-                if (nc == 0U) {
-                    return 1U;
-                } else {
-                    return LegacyIBMCostFunction({qc::X, nc}) + 2U * LegacyIBMCostFunction({qc::U3, 0});
-                }
+                return LegacyIBMCostFunction({qc::X, nc}) + 2U * LegacyIBMCostFunction({qc::U3, 0});
             case qc::SWAP:
                 return LegacyIBMCostFunction({qc::X, nc}) + 2U * LegacyIBMCostFunction({qc::X, 1});
-
             case qc::iSWAP:
                 return LegacyIBMCostFunction({qc::SWAP, nc}) + 2U * LegacyIBMCostFunction({qc::S, nc - 1U}) + LegacyIBMCostFunction({qc::Z, nc});
-
             case qc::Peres:
             case qc::Peresdag:
                 return LegacyIBMCostFunction({qc::X, nc}) + LegacyIBMCostFunction({qc::X, nc - 1U});
-
-            case qc::Compound: // this assumes that compound operations only arise from single qubit fusion
-            case qc::Measure:
-            case qc::Barrier:
-            case qc::ShowProbabilities:
-            case qc::Snapshot:
-                return 1U; // these operations are assumed to incur no cost, but to advance the procedure 1 is used
-
             default:
-                std::cerr << "No cost for gate " << std::to_string(gate) << " -> assuming cost of 1!" << std::endl;
                 return 1U;
         }
     }
