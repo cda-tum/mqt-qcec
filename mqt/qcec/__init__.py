43a8e215dfeef9083eda68d107b9c8f74ef97e52
Lukas Burgholzer <lukas.burgholzer@jku.at>
2022-03-24 23:48:53 +0100

    🚸 make C++ Python module import fully qualified
    Signed-off-by: Lukas Burgholzer <lukas.burgholzer@jku.at>

--- mqt/qcec/__init__.py
+++ mqt/qcec/__init__.py
@@ -3,6 +3,6 @@
 # See file README.md or go to http://iic.jku.at/eda/research/quantum_verification/ for more information.
 #
 
-from .pyqcec import ApplicationScheme, StateType, EquivalenceCriterion, EquivalenceCheckingManager, Configuration
+from mqt.qcec.pyqcec import ApplicationScheme, StateType, EquivalenceCriterion, EquivalenceCheckingManager, Configuration
 
 __all__ = ["ApplicationScheme", "StateType", "EquivalenceCriterion", "EquivalenceCheckingManager", "Configuration"]
