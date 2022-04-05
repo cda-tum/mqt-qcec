90657ec502d2e118de45953c9440a66feb81dc30
Lukas Burgholzer <lukas.burgholzer@jku.at>
2022-04-05 09:20:42 +0200

    🐛 fix python tests
    Signed-off-by: Lukas Burgholzer <lukas.burgholzer@jku.at>

--- test/python/test_construction.py
+++ test/python/test_construction.py
@@ -5,30 +5,32 @@ from qiskit import QuantumCircuit
 
 
 @pytest.fixture
-def example_circuit():
+def example_circuit() -> QuantumCircuit:
     qc = QuantumCircuit(1)
     qc.h(0)
     return qc
 
 
-def test_default_constructor_with_qiskit(example_circuit):
+def test_default_constructor_with_qiskit(example_circuit: QuantumCircuit):
     """Test constructing an instance from two qiskit circuits with all default arguments"""
     qcec.EquivalenceCheckingManager(circ1=example_circuit, circ2=example_circuit)
 
 
-def test_constructor_with_configuration(example_circuit):
+def test_constructor_with_configuration(example_circuit: QuantumCircuit):
     """Test constructing an instance from circuits and a configuration object"""
     config = qcec.Configuration()
     qcec.EquivalenceCheckingManager(circ1=example_circuit, circ2=example_circuit, config=config)
 
 
-def test_constructor_with_kwargs(example_circuit):
+def test_constructor_with_kwargs(example_circuit: QuantumCircuit):
     """Test constructing an instance from circuits and a (random) keyword argument"""
     qcec.EquivalenceCheckingManager(circ1=example_circuit, circ2=example_circuit, timeout=60.)
 
 
-def test_default_constructor_with_file():
+def test_default_constructor_with_file(example_circuit: QuantumCircuit):
     """Test constructing an instance from two circuit files with all default arguments"""
-    file1 = "../circuits/original/mlp4_245.real"
-    file2 = "../circuits/transpiled/mlp4_245_transpiled.qasm"
-    qcec.EquivalenceCheckingManager(circ1=file1, circ2=file2)
+    import os
+    filename = "test.qasm"
+    example_circuit.qasm(filename=filename)
+    qcec.EquivalenceCheckingManager(circ1=filename, circ2=filename)
+    os.remove(filename)
