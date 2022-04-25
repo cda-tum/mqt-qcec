84956f96307f09fb565b3894349de56517584d11
burgholzer <burgholzer@me.com>
2022-04-25 17:27:57 +0200

    🐛 fix repeated build error in setup.py
    Signed-off-by: burgholzer <burgholzer@me.com>

--- setup.py
+++ setup.py
@@ -77,6 +77,8 @@ class CMakeBuild(build_ext):
 
         if not os.path.exists(self.build_temp):
             os.makedirs(self.build_temp)
+        else:
+            os.remove(self.build_temp + "/CMakeCache.txt")
         subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp)
         subprocess.check_call(['cmake', '--build', '.', '--target', ext.name] + build_args, cwd=self.build_temp)
 
