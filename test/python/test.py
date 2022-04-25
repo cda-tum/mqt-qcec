f0bb9a4e96a69f1fb2f57bc72399341c8c5eda23
burgholzer <burgholzer@me.com>
2022-04-25 17:30:44 +0200

    ✅ rewrite test to account for new feature
    Signed-off-by: burgholzer <burgholzer@me.com>

--- test/python/test.py
+++ test/python/test.py
@@ -1,9 +1,9 @@
 from qiskit import QuantumCircuit, transpile
-from qiskit.test.mock import FakeLondon
+from qiskit.test.mock import FakeAthens
 from mqt import qcec
 
 if __name__ == "__main__":
-    # create your quantum circuit
+    # create quantum circuit for GHZ state
     circ = QuantumCircuit(3)
     circ.h(0)
     circ.cx(0, 1)
@@ -11,12 +11,14 @@ if __name__ == "__main__":
     circ.measure_all()
     print(circ.draw(fold=-1))
 
-    # compile circuit to 5 qubit London Architecture
-    circ_comp = transpile(circ, backend=FakeLondon())
+    # compile the circuit to the 5-qubit IBMQ Athens architecture
+    optimization_level = 1
+    circ_comp = transpile(circ, backend=FakeAthens(), optimization_level=optimization_level)
     print(circ_comp.draw(fold=-1))
 
     # initialize the equivalence checker
     ecm = qcec.EquivalenceCheckingManager(circ, circ_comp)
+    qcec.setup_compilation_flow_verification(ecm, optimization_level=optimization_level)
 
     # execute the check
     ecm.run()
