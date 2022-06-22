40a58ad896eae96c1385d2fa3084f0785b84536e
Tom Peham <tom.peham@tum.de>
2022-06-22 15:04:43 +0200

    Added missing ZX file.

---
+++ include/checker/zx/ZXChecker.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "Configuration.hpp"
+#include "EquivalenceCriterion.hpp"
+#include "QuantumComputation.hpp"
+#include "ZXDiagram.hpp"
+#include "checker/EquivalenceChecker.hpp"
+#include "nlohmann/json.hpp"
+
+namespace ec {
+    class ZXEquivalenceChecker: public EquivalenceChecker {
+    public:
+        ZXEquivalenceChecker(const qc::QuantumComputation& qc1, const qc::QuantumComputation& qc2, Configuration configuration) noexcept;
+
+        EquivalenceCriterion run() override;
+
+        void json(nlohmann::json& j) const noexcept override {
+            EquivalenceChecker::json(j);
+        }
+
+    private:
+        zx::ZXDiagram miter;
+    };
+} // namespace ec
