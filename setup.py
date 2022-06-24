8f6b9f82cfacdf3d57a03fda9558e0a425ec8f9f
Lukas Burgholzer <lukas.burgholzer@jku.at>
2022-06-25 00:26:19 +0200

    🔥remove qiskit dependency from `test` extra
    Signed-off-by: Lukas Burgholzer <lukas.burgholzer@jku.at>

--- setup.py
+++ setup.py
@@ -107,7 +107,7 @@ setup(
     package_data={'': ['profiles/*.profile']},
     install_requires=["qiskit-terra~=0.20.2"],
     extras_require={
-        "test": ["pytest~=7.1.1", "qiskit-terra>=0.19.2,<0.21.0"],
+        "test": ["pytest~=7.1.1"],
         "docs": ["sphinx==5.0.2", "sphinx-rtd-theme==1.0.0", "sphinxcontrib-bibtex==2.4.2", "sphinx-copybutton==0.4.0"],
         "dev": ["mqt.qcec[test, docs]"]  # requires Pip 21.2 or newer
     },
