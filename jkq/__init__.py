a4e2cf8edfa8eb2775dc643391263cb3f87517ad
Lukas Burgholzer <lukas.burgholzer@jku.at>
2022-06-24 20:06:35 +0200

    🚨 fix LGTM warning
    Signed-off-by: Lukas Burgholzer <lukas.burgholzer@jku.at>

--- jkq/__init__.py
+++ jkq/__init__.py
@@ -1,5 +1,5 @@
 import warnings
-from mqt import *
+from mqt import qcec
 
 warnings.simplefilter('always', DeprecationWarning)
 warnings.warn('Usage via `import jkq` is deprecated in favor of the new prefix. Please use `import mqt` instead.', DeprecationWarning)
