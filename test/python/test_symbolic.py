300157975381f4b25c01884beaf20c6d11b7df56
Tom Peham <pehamtom@gmx.at>
2022-10-21 14:57:27 +0200

    Improve coverage.

--- test/python/test_symbolic.py
+++ test/python/test_symbolic.py
@@ -173,6 +173,26 @@ def test_verify_compilation_on_optimization_levels(original_circuit: QuantumCirc
     )
 
 
+@pytest.mark.parametrize("optimization_level", [0, 1, 2, 3])
+def test_verify_compilation_on_optimization_levels_config(
+    original_circuit: QuantumCircuit, optimization_level: int
+) -> None:
+    """
+    Test the verification of the compilation of a circuit
+    to the 5-qubit IBMQ Athens architecture with various optimization levels.
+    """
+    config = qcec.Configuration()
+    config.execution.run_zx_checker = False
+    compiled_circuit = transpile(original_circuit, backend=FakeAthens(), optimization_level=optimization_level)
+    result = qcec.verify_compilation(
+        original_circuit, compiled_circuit, optimization_level=optimization_level, configuration=config, timeout=3600
+    )
+    assert (
+        result.equivalence == qcec.EquivalenceCriterion.equivalent
+        or result.equivalence == qcec.EquivalenceCriterion.equivalent_up_to_global_phase
+    )
+
+
 def test_performed_insantiations(rz_commute_lhs: QuantumCircuit, rz_commute_rhs_incorrect: QuantumCircuit) -> None:
     result = qcec.verify(rz_commute_lhs, rz_commute_rhs_incorrect, additional_instantiations=10)
     assert result.equivalence == qcec.EquivalenceCriterion.not_equivalent
