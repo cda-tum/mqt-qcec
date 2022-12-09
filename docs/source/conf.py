09abfeac2e51b2f664a2a5ff73ca82f7206c5487
Lukas Burgholzer <lukas.burgholzer@jku.at>
2022-12-09 15:39:01 +0100

    📝 add dark-mode support and logo to RtD
    Signed-off-by: Lukas Burgholzer <lukas.burgholzer@jku.at>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,6 +32,7 @@ extensions = [
     "sphinx_copybutton",
     "hoverxref.extension",
     "nbsphinx",
+    "sphinx_rtd_dark_mode",
 ]
 
 nbsphinx_execute = "auto"
@@ -80,5 +81,7 @@ autosummary_generate = True
 
 # -- Options for HTML output -------------------------------------------------
 html_theme = "sphinx_rtd_theme"
-
 html_baseurl = "https://qcec.readthedocs.io/en/latest/"
+html_logo = "_static/mqt_light.png"
+html_static_path = ["_static"]
+html_css_files = ["custom.css"]
