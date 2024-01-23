ac4ec3b903d003e494d4756c84a1c59f36567d32
burgholzer <burgholzer@me.com>
2024-01-23 10:44:44 +0100

    🔧 switch external dependency order
    Signed-off-by: burgholzer <burgholzer@me.com>

--- cmake/ExternalDependencies.cmake
+++ cmake/ExternalDependencies.cmake
@@ -3,29 +3,6 @@
 include(FetchContent)
 set(FETCH_PACKAGES "")
 
-if(BUILD_MQT_QCEC_TESTS)
-  set(gtest_force_shared_crt
-      ON
-      CACHE BOOL "" FORCE)
-  set(GTEST_VERSION
-      1.14.0
-      CACHE STRING "Google Test version")
-  set(GTEST_URL
-      https://github.com/google/googletest/archive/refs/tags/v${GTEST_VERSION}.tar.gz
-  )
-  if(CMAKE_VERSION VERSION_GREATER_EQUAL 3.24)
-    FetchContent_Declare(googletest URL ${GTEST_URL} FIND_PACKAGE_ARGS
-                                        ${GTEST_VERSION} NAMES GTest)
-    list(APPEND FETCH_PACKAGES googletest)
-  else()
-    find_package(googletest ${GTEST_VERSION} QUIET NAMES GTest)
-    if(NOT googletest_FOUND)
-      FetchContent_Declare(googletest URL ${GTEST_URL})
-      list(APPEND FETCH_PACKAGES googletest)
-    endif()
-  endif()
-endif()
-
 if(BUILD_MQT_QCEC_BINDINGS)
   # Manually detect the installed mqt-core package.
   execute_process(
@@ -73,6 +50,29 @@ else()
       GIT_REPOSITORY https://github.com/cda-tum/mqt-core.git
       GIT_TAG v${MQT_CORE_VERSION})
     list(APPEND FETCH_PACKAGES mqt-core)
+  endif()
+endif()
+
+if(BUILD_MQT_QCEC_TESTS)
+  set(gtest_force_shared_crt
+      ON
+      CACHE BOOL "" FORCE)
+  set(GTEST_VERSION
+      1.14.0
+      CACHE STRING "Google Test version")
+  set(GTEST_URL
+      https://github.com/google/googletest/archive/refs/tags/v${GTEST_VERSION}.tar.gz
+  )
+  if(CMAKE_VERSION VERSION_GREATER_EQUAL 3.24)
+    FetchContent_Declare(googletest URL ${GTEST_URL} FIND_PACKAGE_ARGS
+                                        ${GTEST_VERSION} NAMES GTest)
+    list(APPEND FETCH_PACKAGES googletest)
+  else()
+    find_package(googletest ${GTEST_VERSION} QUIET NAMES GTest)
+    if(NOT googletest_FOUND)
+      FetchContent_Declare(googletest URL ${GTEST_URL})
+      list(APPEND FETCH_PACKAGES googletest)
+    endif()
   endif()
 endif()
 
