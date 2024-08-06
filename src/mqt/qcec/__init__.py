1d550a1e602dced0d378fa52ba8356ba7a7fca4c
burgholzer <burgholzer@me.com>
2024-08-06 16:46:55 +0200

    ♻️ make qiskit an optional dependency
    Signed-off-by: burgholzer <burgholzer@me.com>

--- src/mqt/qcec/__init__.py
+++ src/mqt/qcec/__init__.py
@@ -7,7 +7,6 @@ See README.md or go to https://github.com/cda-tum/qcec for more information.
 from __future__ import annotations
 
 from ._version import version as __version__
-from .compilation_flow_profiles import AncillaMode, generate_profile
 from .pyqcec import (
     ApplicationScheme,
     Configuration,
@@ -16,17 +15,24 @@ from .pyqcec import (
     StateType,
 )
 from .verify import verify
-from .verify_compilation_flow import verify_compilation
+
+# Conditionally import functionality depending on the availability of Qiskit
+try:
+    from .compilation_flow_profiles import AncillaMode, generate_profile
+    from .verify_compilation_flow import verify_compilation
+
+    qiskit_installed = True
+except ImportError:
+    qiskit_installed = False
 
 __all__ = [
-    "AncillaMode",
     "ApplicationScheme",
     "Configuration",
     "EquivalenceCheckingManager",
     "EquivalenceCriterion",
     "StateType",
     "__version__",
-    "generate_profile",
     "verify",
-    "verify_compilation",
 ]
+if qiskit_installed:
+    __all__ += ["AncillaMode", "generate_profile", "verify_compilation"]
