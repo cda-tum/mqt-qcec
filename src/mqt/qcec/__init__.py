c977c1e78c2386eb84717b83632c62452eaf8f62
burgholzer <burgholzer@me.com>
2024-08-07 09:54:37 +0200

    🏁 patch dll directory on Windows
    Signed-off-by: burgholzer <burgholzer@me.com>

--- src/mqt/qcec/__init__.py
+++ src/mqt/qcec/__init__.py
@@ -6,6 +6,23 @@ See README.md or go to https://github.com/cda-tum/qcec for more information.
 
 from __future__ import annotations
 
+import sys
+
+# under Windows, make sure to add the appropriate DLL directory to the PATH
+if sys.platform == "win32":
+
+    def _dll_patch() -> None:
+        """Add the DLL directory to the PATH."""
+        import os
+        import sysconfig
+        from pathlib import Path
+
+        bin_dir = Path(sysconfig.get_paths()["purelib"]) / "mqt" / "core" / "bin"
+        os.add_dll_directory(str(bin_dir))
+
+    _dll_patch()
+    del _dll_patch
+
 from ._version import version as __version__
 from .compilation_flow_profiles import AncillaMode
 from .pyqcec import (
